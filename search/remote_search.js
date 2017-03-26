daf62106390889f118deaf476b2ffdc60e67a925
danielchase93 <danielchase93@gmail.com>
2017-03-26 01:57:54 -0600

    update 3/25/17

--- search/remote_search.js
+++ search/remote_search.js
@@ -9,7 +9,7 @@ $('#query').keyup(function() {
         $.each(data.RESULTS, function(key, val) {
             if (val.name.search(rExp) != -1) {
                 output += '<li>';
-                output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+                output += '<a href="#"' + 'onclick="getData('+ val.lat + ',' + val.lon +')"' + 'title="See results for' + val.name +'">' + val.name + '</a>';
                 output += '</li>';
             }
         });
@@ -18,3 +18,46 @@ $('#query').keyup(function() {
         $("#searchResults").html(output);
     });
 });
+
+
+
+
+
+function getData(lat, lon){
+    $.ajax({
+        url : "https://api.wunderground.com/api/c12cbe7add4cc97b/geolookup/conditions/forecast/q/"+lat+","+lon+".json",
+        dataType:"jsonp",
+        success: function(data){
+            console.log(data);
+
+
+            $("#cityDisplay").html(data.location.city + ", " +  data.location.state);
+
+            $(".currentTemp").html(Math.round(data.current_observation.temp_f) + "&deg" + "F");
+
+            $("#summary").html(data.current_observation.weather);
+
+            $("#high").html("High: " +  data.forecast.simpleforecast.forecastday["0"].high.fahrenheit + "&deg" + "F");
+
+            $("#low").html("Low: " +  data.forecast.simpleforecast.forecastday["0"].low.fahrenheit + "&deg" + "F");
+
+            $("#feels").html("Feels Like: " + Math.round(data.current_observation.feelslike_f) + "&deg" + "F");
+
+            $("#long").html("Longitude: " + lon);
+            $("#lat").html("Latitude: " + lat);
+
+
+
+
+
+
+            $("#cover").fadeOut(250);
+        }
+    });
+
+}
+
+
+
+
+
